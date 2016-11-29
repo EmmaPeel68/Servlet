19ba74c8a13fb9fc06207ea771e916a0943352d9
“Paty <patyanadon@hotmail.com>
2016-11-29 13:10:20 +0100

    Nuevos cambios

---
+++ ServletJsp/src/main/java/servlet/WelcomeServlet.java
@@ -0,0 +1,30 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Form;
+import service.Service;
+
+public class WelcomeServlet extends HttpServlet{
+
+	private Service service = new Service();
+	
+	@Override
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		Form form = service.assembleFormFromRequest(req);
+		service.insert(form);
+		redirect(req,resp);
+	}
+	
+	protected void redirect(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/fin.jsp");
+		dispatcher.forward(req,resp);
+	}
+	
+}
--- ServletJsp/src/main/webapp/fin.jsp
+++ ServletJsp/src/main/webapp/fin.jsp
@@ -4,7 +4,7 @@
 <%@ page import="java.io.*,java.util.*,model.*" %>
 
 
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!--<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>-->
 
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
